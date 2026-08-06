4ccf48413693d190b3798dc20f4bb08f5da5d5da
Joel Simon <joelsmn17@gmail.com>
2026-08-06 14:32:24 +0530

    Time: 1223 ms (5.07%) | Memory: 20.4 MB (28.24%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+
+        nums=sorted(nums)
+        print(nums)
+        return nums[-k]
+        
